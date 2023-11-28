25797c723f69710568ec0c6e2d68b92f7f4dd127
Rodrigo Facundo <frodzek@proton.me>
2023-11-28 18:16:32 -0300

    nuevo menu

--- script.js
+++
@@ -1,9 +0,0 @@
-function abrirVentana() {
-    var ventanaEmergente = document.querySelector('.register');
-    ventanaEmergente.style.display = 'flex';
-}
-
-function cerrarVentana() {
-    var ventanaEmergente = document.querySelector('.register');
-    ventanaEmergente.style.display = 'none';
-}
